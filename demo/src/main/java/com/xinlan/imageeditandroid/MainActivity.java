e070a89c916fe13512f1cd01d2cec73b0a4d10af
Vitor Moraes <vitordmoraes.93@gmail.com>
2024-09-08 17:56:33 -0300

    fixes

--- demo/src/main/java/com/xinlan/imageeditandroid/MainActivity.java
+++ demo/src/main/java/com/xinlan/imageeditandroid/MainActivity.java
@@ -97,9 +97,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void requestTakePhotoPermissions() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_VIDEO)
+                        != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES)
+                        != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA,Manifest.permission.MANAGE_EXTERNAL_STORAGE},
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_MEDIA_VIDEO,
+                            Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_AUDIO},
                     REQUEST_PERMISSON_CAMERA);
             return;
         }
@@ -113,14 +120,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             File photoFile = FileUtils.genEditFile();
-            // Continue only if the File was successfully created
             if (photoFile != null) {
-                photoURI = Uri.fromFile(photoFile);
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.xinlan.imageeditandroid.fileprovider",
+                        photoFile);
+
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
                 startActivityForResult(takePictureIntent, TAKE_PHOTO_CODE);
             }
-
-            //startActivityForResult(takePictureIntent, TAKE_PHOTO_CODE);
         }
     }
 
@@ -152,10 +162,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void openAblumWithPermissionsCheck() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
+        // Verifica se todas as permissões necessárias foram concedidas
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES)
+                        != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_VIDEO)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            // Solicita as permissões necessárias
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.MANAGE_EXTERNAL_STORAGE},
+                    new String[]{
+                            Manifest.permission.CAMERA,
+                            Manifest.permission.READ_MEDIA_AUDIO,
+                            Manifest.permission.READ_MEDIA_IMAGES,
+                            Manifest.permission.READ_MEDIA_VIDEO
+                    },
                     REQUEST_PERMISSON_SORAGE);
             return;
         }
